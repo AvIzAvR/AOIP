019e6f6344393a8c697b3a92710450da0ef4fedf
AvIzAvR <108844960+AvIzAvR@users.noreply.github.com>
2022-09-20 18:55:40 +0300

    Add files via upload

---
+++ 2.3.2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int C = 0;
+	float A = 0, B = 0;
+	printf("Введите больший отрезок A:\n");
+	scanf_s("%f", &A);
+	printf("Введите больший отрезок B:\n");
+	scanf_s("%f", &B);
+	if (A > B)
+	{
+		for (; A >= B; A -= B)
+		{
+			++C;
+		}
+		printf("Количество отрезков, которое удалось разместить: %d\nНезанятая часть отрезка A равна: %f", C, A);
+	}
+	else
+	{
+		printf("Неверный тип данных");
+	}
+	return 0;
+}
