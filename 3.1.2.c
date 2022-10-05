dbda22f79241f470d8b24f9cad67843c86cbbf93
AvIzAvR <108844960+AvIzAvR@users.noreply.github.com>
2022-10-06 00:50:07 +0300

    Update 3.1.2.c

--- 3.1.2.c
+++ 3.1.2.c
@@ -1,17 +1,15 @@
-##include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	while (1)
+	while(1)
 	{
-		printf("Введите размерность для двух массивов:\nДля случайной генерации размерности введите \"r\"\n");
+		printf("Введите размерность массива:\nДля случайной генерации размерности введите \"r\"\n");
 		int mass[100];
-		int mass2[100];
-		int true, raz = 10;
-		int temp = -1;
-		int torf = 0;
+		int raz, nul1, nul2, pred, true, rands;
+		int brain = 1;
 		scanf_s("%d", &raz);
 		if (raz > 0)
 		{
@@ -38,13 +36,37 @@ int main()
 			printf("Неверно введённый тип данных.");
 			exit(0);
 		}
-		for (int i = 0; i <= raz; i++)
+
+		printf("Если хотите продолжить ввод чисел с клавиатуры введите \"1\"\nЕсли хотите сгенерировать %d случайных чисел введите \"2\"\n", raz);
+		scanf_s("%d", &rands);
+		if (rands == 1)
 		{
-			printf("Введите %d-е число 1-ого массива\n", i);
-			scanf_s("%d", &true);
-			if (true >= 0 || true < 0)
+			for (int i = 0; i <= raz; i++)
 			{
-				mass[i] = true;
+				printf("Введите %d-е число массива\n", i);
+				scanf_s("%d", &true);
+				if (true >= 0 || true < 0)
+				{
+					mass[i] = true;
+				}
+				else
+				{
+					printf("Неверно введённый тип данных.");
+					exit(0);
+				}
+			}
+		}
+		else if (rands == 2)
+		{
+			printf("Введите максимальный предел для всех чисел:\n");
+			scanf_s("%d", &pred);
+			if (pred > 0)
+			{
+				for (int i = 0; i <= raz; i++)
+				{
+					mass[i] = rand() % pred;
+					printf("%d; ", mass[i]);
+				}
 			}
 			else
 			{
@@ -52,40 +74,50 @@ int main()
 				exit(0);
 			}
 		}
+		else
+		{
+			printf("Неверно введённый тип данных.");
+			exit(0);
+		}
+		for (int i = 1; i <= raz; i++)
+		{
+			if (i % 2 == 0)
+			{
+				brain *= mass[i];
+			}
+		}
+		printf("\nПроизведение элементов массива с четными номерами: %d\n", brain);
+		int nul = 0;
 		for (int i = 0; i <= raz; i++)
 		{
-			printf("Введите %d-е число 2-ого массива\n", i);
-			scanf_s("%d", &true);
-			if (true >= 0 || true < 0)
+			if (mass[i] == 0)
 			{
-				mass2[i] = true;
-			}
-			else
-			{
-				printf("Неверно введённый тип данных.");
-				exit(0);
+				nul++;
 			}
 		}
-
-
-		for (int i = 0; i < raz; ++i)
+		if (nul > 1)
 		{
-			torf = 0;
-			for (int j = 0; j < raz; ++j)
+			for (int i = 0; i <= raz; i++)
 			{
-				if (mass[i] == mass2[j])
+				if (mass[i] == 0)
 				{
-					torf = 1;
-				}
-			}
-			if (torf == 0)
-			{
-				if (temp == -1 || temp > mass[i])
-				{
-					temp = mass[i];
+					nul1 = mass[i + 1];
+					for (int ii = raz; ii > 0; ii--)
+					{
+						if (mass[ii] == 0)
+						{
+							nul2 = mass[ii - 1];
+							printf("Сумму элементов массива, расположенных между первым и последним нулевыми элементами: %d\n", (nul1 + nul2));
+							break;
+						}
+					}
+					break;
 				}
 			}
 		}
-		printf("Результат: %d", temp);
+		else
+		{
+			printf("Двух и более нулей в массиве не обнаружено\n");
+		}
 	}
 }
