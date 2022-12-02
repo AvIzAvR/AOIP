12043a01cf2d7a6946c52bc54d35a161bd97b2b0
AvIzAvR <108844960+AvIzAvR@users.noreply.github.com>
2022-12-02 22:38:01 +0300

    Update line.cpp

--- 7/7/line.cpp
+++ 7/7/line.cpp
@@ -9,52 +9,77 @@
 #include"lineLib.h"
 
 
-void Task1(int argc, char** argv);
-void Task2(int argc, char** argv);
-void Task3(int argc, char** argv);
+void task1(int argc, char** argv);
+void task2(int argc, char** argv);
+void task3(int argc, char** argv);
+void stop(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-	char* tasks;
 	int task = 0;
 	setlocale(LC_ALL, "Rus");
-	menu();
-	razfortask(&task);
-	void (*taskss[3]) (int, char**) = { Task1, Task2, Task3};
-	((*taskss[task - 1])(argc, argv));
+		menu();
+		getIntfortask(&task);
+		void (*taskss[4]) (int, char**) = { task1, task2, task3, stop };
+		((*taskss[task - 1])(argc, argv));
 }
 
+void stop(int argc, char** argv)
+{
+	exit(EXIT_SUCCESS);
+}
 
-void Task1(int argc, char** argv)
+void task1(int argc, char** argv)
 {
 	printf("Введите строку, в которой хотите преобразовать все первые буквы каждого слова в строчные:\n");
-	char* string = getstringWithFirstUp();
-	printf("%s", string);
+	char* string = getString();
+	string = downFirst(string);
+	printf("Результат:");
+	printf("%s\n", string);
+	free(string);
 }
 
 
-void Task2(int argc, char** argv)
+void task2(int argc, char** argv)
 {
 	int N1 = 0, N2 = 0;
-	printf("Введите N1:\n"); 
-	raz(&N1);
+	printf("Введите N1:\n");
+	getInt(&N1);
 	printf("Введите N2:\n");
-	raz(&N2);
+	getInt(&N2);
 	printf("Введите первую строку:\n");
-	char* string = getstring();
+	char* string = getString();
 	printf("Введите вторую строку:\n");
-	char* string1 = getstring();
-	char* string3 = newString(N1, N2, string, string1);
-	printf("Результат:");
-	printf("%s", string3);
+	char* string1 = getString();
+	int size = getSize(string);
+	int size1 = getSize(string1);
+	if (size < N1 || size1 < N2)
+		printf("Неверный тип данных!\n");
+	else
+	{
+		char* string3 = newString(N1, N2, string, string1);
+		printf("Результат:\n");
+		printf("%s\n", string3);
+		free(string3);
+	}
+	free(string);
+	free(string1);
 }
 
-void Task3(int argc, char** argv)
+void task3(int argc, char** argv)
 {
-	if (argc == 1) {
-		printf_s("\nНеверное кол-во аргов\n");
+	if (argc == 1)
+	{
+		printf_s("\nНеверное кол-во аргументов\n");
 		return;
 	}
 	char** strings = (char**)calloc(argc - 1, sizeof(char*));
-	printf("%s", strings);
+	int* sizestring = (int*)calloc((argc - 1), sizeof(int));
+	for(int i = 1; i < argc; i++)
+	copyString(argv[i], strings[i-1]);
+	getSizeString(argc, sizestring, strings);
+	quickSort(sizestring, strings, 0, (argc - 1));
+	printStrings(argc, strings);
+	free(sizestring);
+	free_strings(strings, argc);
 }
