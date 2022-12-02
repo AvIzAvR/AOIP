eb42a85a5eb5d1ef8fdec75e68ca6153262e716a
AvIzAvR <108844960+AvIzAvR@users.noreply.github.com>
2022-12-02 22:38:36 +0300

    Update lineLib.h

--- 7/7/lineLib.h
+++ 7/7/lineLib.h
@@ -1,12 +1,19 @@
 #ifndef lineLib
 #define lineLib
 
-void raz(int* x);
+void getInt(int* x);
 void menu();
-void razfortask(int* task);
-int whensize(char* string);
-char* getstring();
-char* getstringWithFirstUp();
+void getIntfortask(int* task);
+int getSize(char* string);
+char* getString();
 char* newString(int N1, int N2, char* string, char* string1);
+void quickSort(int*& arr, char**& sort, int start, int end);
+void copyString(char* argv, char*& string);
+void getSizeString(int argc, int*& sizestring, char** strings);
+int getSizeword(char* str, char letter);
+char* downFirst(char* string);
+void printStrings(int argc, char** strings);
+void free_strings(char** strings, int argc);
+
 
 #endif
